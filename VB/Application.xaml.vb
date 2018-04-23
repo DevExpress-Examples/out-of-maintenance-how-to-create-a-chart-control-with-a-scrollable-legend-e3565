31cf68ef53fe67db43290e866299023520c284d5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Application.xaml.vb
@@ -0,0 +1,27 @@
+﻿' Developer Express Code Central Example:
+' How to create a chart control with a scrollable legend
+' 
+' This example illustrates how to create a chart control with a scrollable legend.
+' This may be required, for instance, when a chart displays too many series, or a
+' legend occupies too much space.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E3565
+
+
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Configuration
+Imports System.Data
+Imports System.Linq
+Imports System.Windows
+
+Namespace ScrollableLegend
+	''' <summary>
+	''' Interaction logic for App.xaml
+	''' </summary>
+	Partial Public Class App
+		Inherits Application
+	End Class
+End Namespace
